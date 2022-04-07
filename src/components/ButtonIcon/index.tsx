3bb096396bc56ceeece384b7b34f98905a412eb9
Isaac silva <Isaac silva>
2022-04-06 23:56:50 -0300

    Custom ButtonIcon

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,11 +1,17 @@
 import './styles.css';
+import { ReactComponent as ArrowIcon } from 'assets/Images/arrow.svg';
 
 const ButtonIcon = () => {
     return (
-      <button className="btn btn-primary btn-icon">
-          <h6>INICIE AGORA SUA BUSCA</h6>
-
+      <div className="btn-container">
+         <button className="btn btn-primary btn-icon">
+          <h6>inicie agora sua busca</h6>
       </button>
+          <div className="btn-icon-container">
+          <ArrowIcon />  
+          </div>
+      </div>
+      
        
     );
   }
